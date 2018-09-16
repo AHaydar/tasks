636e4b393252f89fd4790cf8bf8960d605846a42
Ali Haydar <a.haydar85@gmail.com>
2018-09-16 16:06:49 +1200

    Add a simple static tasks page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+        <div class="col-md-6">
+            <div class="todolist">
+                <h1>Tasks</h1>
+                <hr>
+                <ul>
+                    <li>Take out the trash</li>
+                    <li>Buy bread</li>
+                    <li>Watch Atypical</li>
+                </ul>
+            </div>
+        </div>
+@endsection
+
