94f46f1f8b1b7ed44ba328007448f159a30b006c
Ali Haydar <a.haydar85@gmail.com>
2018-09-23 19:50:54 +1200

    Add new tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Task;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
@@ -23,5 +24,19 @@ class TasksController extends Controller
         $userId = Auth::user()->id;
         $tasks = Task::all()->where('user_id', $userId);
         return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $task = new Task();
+        $task->title = $request->input('title');
+        $task->user_id = Auth::user()->id;
+        $task->completed = false;
+
+        $task->save();
+
+        return redirect('/tasks');
+
     }
 }
